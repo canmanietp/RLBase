8d065bbe4c2e5db26c0dd9211507a7b80a2ef5cf
canmanie <c.t.ponnambalam@tudelft.nl>
2020-03-10 10:59:52 +0100

    debugging

---
+++ src/envs/__init__.py
@@ -0,0 +1,15 @@
+from gym.envs.registration import register
+
+register(
+    id='CartPole-v0',
+    entry_point='gym.envs.classic_control:CartPoleEnv',
+    max_episode_steps=200,
+    reward_threshold=195.0,
+)
+
+register(
+    id='CartPole-v1',
+    entry_point='gym.envs.classic_control:CartPoleEnv',
+    max_episode_steps=500,
+    reward_threshold=475.0,
+)
