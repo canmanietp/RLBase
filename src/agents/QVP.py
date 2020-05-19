8ca8a05688dcfe7bc5b8ad8e4f9be58db878a3e2
canmanie <c.t.ponnambalam@tudelft.nl>
2020-05-19 13:08:33 +0200

    update

--- src/agents/QVP.py
+++ src/agents/QVP.py
@@ -164,7 +164,7 @@ class QVPAgent(QAgent):
                         b = np.array([item[a] for item in merge_visits])
                         most_visited = int(np.random.choice(np.flatnonzero(b == b.max())))
                         qs[a] = merge_values[most_visited][a]
-                return None, np.argmax(qs)
+            return None, np.argmax(qs)
         else:
             return None, np.argmax(self.Q_table[state])
 
