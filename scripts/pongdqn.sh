116e7721e443503962439d442d679d9de149cd81
canmanie <c.t.ponnambalam@tudelft.nl>
2020-06-11 14:35:21 +0200

    adding script

---
+++ scripts/pongdqn.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#SBATCH —partition=general
+#SBATCH —qos=short
+#SBATCH —time=60:00:00
+#SBATCH —ntasks=1
+#SBATCH —gres=gpu:2
+#SBATCH —mem=4096
+#SBATCH —mail-type=END
+module use /opt/insy/modulefiles
+module load cuda/10.0 cudnn/10.0-7.3.0.29
+
+cd ..
+cd src
+
+srun python3 run.py —algorithms “DQN” —env=pong
