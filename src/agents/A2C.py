90d9022d8c90e659b5f5c0b71a769c6deba1d9d7
canmanie <c.t.ponnambalam@tudelft.nl>
2020-06-17 15:08:18 +0200

    fixing a2c

--- src/agents/A2C.py
+++ src/agents/A2C.py
@@ -165,7 +165,10 @@ class A2CAgent(BaseAgent):
 
     def replay(self):
         returns, advantages = get_advantages(self.values, self.masks, self.rewards)
-        print("replay", returns, advantages[:, 0])
+        reshaped_ad = []
+        for a in advantages:
+            reshaped_ad.append(a[0])
+        print(reshaped_ad)
         actor_loss = self.model_actor.fit(
             [self.states, self.actions_probs, advantages, np.reshape(self.rewards, newshape=(-1, 1, 1)), self.values[:-1]],
             [(np.reshape(self.actions_onehot, newshape=(-1, self.action_space)))], verbose=False, shuffle=True, epochs=8)
