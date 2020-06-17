0309cae330ca68a821e1d84fd74f799911a202a2
canmanie <c.t.ponnambalam@tudelft.nl>
2020-06-17 12:11:09 +0200

    fixing a2c

--- src/agents/A2C.py
+++ src/agents/A2C.py
@@ -115,14 +115,15 @@ class A2CAgent(BaseAgent):
         return model
 
     def remember(self, state, action, reward, done):
+        state_input = np.expand_dims(state, axis=0)
         print('remember', state.shape)
-        action_dist = self.model_actor.predict([state, self.dummy_n, self.dummy_1, self.dummy_1, self.dummy_1], steps=1)
+        action_dist = self.model_actor.predict([state_input, self.dummy_n, self.dummy_1, self.dummy_1, self.dummy_1], steps=1)
         action_onehot = np.zeros(self.action_space)
         action_onehot[action] = 1
         self.states.append(state)
         self.actions.append(action)
         self.actions_onehot.append(action_onehot)
-        q_value = self.model_critic.predict([state], steps=1)
+        q_value = self.model_critic.predict([state_input], steps=1)
         self.values.append(q_value)
         self.masks.append(not done)
         self.rewards.append(reward)
@@ -142,7 +143,6 @@ class A2CAgent(BaseAgent):
             next_state, reward, done, next_state_info = self.env.step(action)
 
         state = np.reshape(self.last_n_states, self.input_shape)
-        state_input = np.expand_dims(state, axis=0)
         print('step', state_input.shape)
 
         self.current_state = next_state
