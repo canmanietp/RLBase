e6f029ae0cbef8e3a904eda9a01a2e9442d23cd0
canmanie <c.t.ponnambalam@tudelft.nl>
2020-06-17 15:11:53 +0200

    fixing a2c

--- src/agents/A2C.py
+++ src/agents/A2C.py
@@ -170,7 +170,7 @@ class A2CAgent(BaseAgent):
             reshaped_ad.append(a[0][0])
         print(reshaped_ad)
         actor_loss = self.model_actor.fit(
-            [self.states, self.actions_probs, reshaped_ad, np.reshape(self.rewards, newshape=(-1, 1, 1)), self.values[:-1]],
+            [self.states, self.actions_probs, K.expand_dims(reshaped_ad, 0), np.reshape(self.rewards, newshape=(-1, 1, 1)), self.values[:-1]],
             [(np.reshape(self.actions_onehot, newshape=(-1, self.action_space)))], verbose=False, shuffle=True, epochs=8)
         critic_loss = self.model_critic.fit([self.states], [np.reshape(returns, newshape=(-1, 1))], shuffle=True, epochs=8,
                                        verbose=False)
