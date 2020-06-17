35090973d9b77846c7a16bd7388f13c3f2ae8d13
canmanie <c.t.ponnambalam@tudelft.nl>
2020-06-17 16:54:38 +0200

    fixing a2c

--- src/agents/A2C.py
+++ src/agents/A2C.py
@@ -169,11 +169,11 @@ class A2CAgent(BaseAgent):
 
     def replay(self):
         returns, advantages = get_advantages(self.values, self.masks, self.rewards)
-        print(returns, np.reshape(returns, newshape=(-1, 1)))
+        print(np.squeeze(returns, axis=1).shape)
         actor_loss = self.model_actor.fit(
             [self.states, self.actions_probs, np.squeeze(advantages, axis=1), np.reshape(self.rewards, newshape=(-1, 1, 1)), np.squeeze(self.values[:-1], axis=1)],
             [(np.reshape(self.actions_onehot, newshape=(-1, self.action_space)))], verbose=False, shuffle=True, epochs=8, callbacks=[self.tensor_board])
-        critic_loss = self.model_critic.fit([self.states], [np.reshape(returns, newshape=(-1, 1))], shuffle=True, epochs=8,
+        critic_loss = self.model_critic.fit([self.states], [np.squeeze(returns, axis=1)], shuffle=True, epochs=8,
                                        verbose=False, callbacks=[self.tensor_board])
 
         print(actor_loss, critic_loss)
