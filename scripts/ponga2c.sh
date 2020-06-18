bec5fb92b1bfa7025c3d879abbb7cda51edeabd1
canmanie <c.t.ponnambalam@tudelft.nl>
2020-06-18 15:23:41 +0200

    fixing a2c

--- scripts/ponga2c.sh
+++ scripts/ponga2c.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=2:00:00:00
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:4
-#SBATCH --mem=4096
+#SBATCH --mem=8192
 #SBATCH --mail-type=END
 module use /opt/insy/modulefiles
 module load cuda/10.0 cudnn/10.0-7.3.0.29
