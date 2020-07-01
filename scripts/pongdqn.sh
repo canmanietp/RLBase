0efc429a3d181f1647d56eb072573c98c83eff35
canmanietp <c.t.ponnambalam@tudelft.nl>
2020-07-01 12:07:50 +0200

    dqn update

--- scripts/pongdqn.sh
+++ scripts/pongdqn.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #SBATCH --partition=general
 #SBATCH --qos=long
-#SBATCH --time=10:00:00
+#SBATCH --time=1440:00:00
 #SBATCH --ntasks=1
-#SBATCH --gres=gpu:2
-#SBATCH --mem=1024
+#SBATCH --gres=gpu:6
+#SBATCH --mem=4096
 #SBATCH --mail-type=END
 module use /opt/insy/modulefiles
 module load cuda/10.0 cudnn/10.0-7.3.0.29
@@ -12,5 +12,5 @@ module load cuda/10.0 cudnn/10.0-7.3.0.29
 cd ..
 cd src
 
-srun python3 run.py --algorithms 'DQN' --env=pong --verbose=TrueE
+srun python3 run.py --algorithms 'DQN' --env=pong --verbose=True
 srun hostname
